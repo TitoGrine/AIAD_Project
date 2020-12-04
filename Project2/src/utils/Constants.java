950619eab62d08ad3758c8dd50c6742ab039c27b
Nokas <up201605335@fe.up.pt>
2020-12-04 12:08:09 +0000

    added parameters to gui

--- Project2/src/utils/Constants.java
+++ Project2/src/utils/Constants.java
@@ -42,14 +42,14 @@ public class Constants {
     public final static int[] CAPACITY_DISTRIBUTION = {30, 100};            // Range of maximum capacities of vehicles in kWh
     public final static double CHARGING_PRICE = 1.0;                        // Price to charge vehicle per kWh
 
-    public final static double[] VEHICLE_DISTRIBUTION = {0.0, 0.5, 0.5};    // Relative frequency of each type of vehicle
-    public final static double MEAN_ALTRUISTIC_VALUE = 0.5;                 // Mean value of altruistic factor of normal distribution
-    public final static double ALTRUISTIC_STANDARD_DEVIATION = 0.5;         // Standard deviation of altruistic factor normal distribution
-    public final static double MEAN_PERMISSION_VALUE = 0.5;                 // Mean value of normal distribution for permission to charge grid (lower mean means higher probability of acceptance)
-    public final static double PERMISSION_STANDARD_DEVIATION = 0.2;         // Standard deviation of normal distribution for permission to charge grid
+    public static double[] VEHICLE_DISTRIBUTION = {0.0, 0.5, 0.5};    // Relative frequency of each type of vehicle
+    public static double MEAN_ALTRUISTIC_VALUE = 0.5;                 // Mean value of altruistic factor of normal distribution
+    public static double ALTRUISTIC_STANDARD_DEVIATION = 0.5;         // Standard deviation of altruistic factor normal distribution
+    public static double MEAN_PERMISSION_VALUE = 0.5;                 // Mean value of normal distribution for permission to charge grid (lower mean means higher probability of acceptance)
+    public static double PERMISSION_STANDARD_DEVIATION = 0.2;         // Standard deviation of normal distribution for permission to charge grid
 
-    public final static int[] CURRENT_DEMAND = SUMMER_GRID_DEMAND;          // The demand simulation (choose from one of the four seasons)
-    public final static int MAX_AVAILABLE_LOAD = SUMMER_MAX_AVAILABLE_LOAD; // The max available load to the system (choose the same season as the one above)
+    public static int[] CURRENT_DEMAND = SUMMER_GRID_DEMAND;          // The demand simulation (choose from one of the four seasons)
+    public static int MAX_AVAILABLE_LOAD = SUMMER_MAX_AVAILABLE_LOAD; // The max available load to the system (choose the same season as the one above)
 
     // Colors
     public static final String RESET = "\u001B[0m";
